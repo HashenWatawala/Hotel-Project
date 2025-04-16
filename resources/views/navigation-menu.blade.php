8a5d3f0779362fd05a50797ae7982ec07101d7d8
HashenWatawala <hashanewatawala@gmail.com>
2025-04-16 10:58:20 +0530

    Add admin page

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -77,9 +77,7 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                        
 
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
@@ -90,8 +88,6 @@
                                     {{ __('API Tokens') }}
                                 </x-jet-dropdown-link>
                             @endif
-
-                            <div class="border-t border-gray-100"></div>
 
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
