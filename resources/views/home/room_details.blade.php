a1dba560f3f3d61ca69938f4becd7a441bd83af0
HashenWatawala <hashanewatawala@gmail.com>
2025-05-16 21:54:00 +0530

    Add js

--- resources/views/home/room_details.blade.php
+++ resources/views/home/room_details.blade.php
@@ -224,6 +224,27 @@
            </div>
         </div>
      </footer>
+     <script type="text/javascript">
+     $(function(){
+      var dtToday = new Date();
+
+      var month = dtToday.getMonth() + 1;
+
+      var day = dtToday.getDate();
+
+      var year = dtToday.getFullYear();
+
+      if(month < 10)
+         month = '0' + month.toString();
+      if(day < 10)
+         day = '0' + day.toString();
+
+      var maxDate = year + '_' + month + '_' + day;
+      $('#startDate').attr('min', maxData);
+      $('#endData').atrr('min', maxData);
+
+     }) 
+     </script>
      <script src="js/jquery.min.js"></script>
       <script src="js/bootstrap.bundle.min.js"></script>
       <script src="js/jquery-3.0.0.min.js"></script>
